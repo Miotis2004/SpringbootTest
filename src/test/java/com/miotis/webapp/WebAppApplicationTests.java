dd14c756d62fc5a19299e57c5ff859e1ec93f5af
Miotis2004 <miotis2004@gmail.com>
2020-09-01 19:31:02 -0700

    initial commit

---
+++ src/test/java/com/miotis/webapp/WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miotis.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
